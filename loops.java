4cbe72ca70cabe1829fc3e43e6339f242d7cfedb
Ketaki Chandak <159540699+ketaki1904@users.noreply.github.com>
2024-10-03 22:28:23 +0530

    loop.java

--- loops.java
+++ loops.java
@@ -14,60 +14,60 @@ public class loops {
         for (int i = 1; i <= 10; i++) {
             System.out.println(n * i);
         }
-        // int n = 5;
+         int n = 5;
 
-        // for (int i = 1; i <= n; i++) {
-        // for (int j = 1; j <= i; j++) {
-        // int sum = i + j;
-        //
-        // if (sum % 2 == 0) {
-        // System.out.print("1 ");
-        //
-        // } else {
-        //
-        // }
-        /// System.out.print("0 ");
-        // }
-        // System.out.println();
-        // }
+         for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+         int sum = i + j;
+        
+         if (sum % 2 == 0) {
+         System.out.print("1 ");
+        
+        } else {
+        
+         }
+         System.out.print("0 ");
+         }
+         System.out.println();
+         }
     }
 
-    /*
-     * if (i == 1 || j == 1 || i == n || j == n) {
-     * System.out.print("*");
-     * } else {
-     * System.out.print(" ");
-     * 
-     * }
-     * }
-     * System.out.println();
-     * 
-     * }
-     * /*
-     * Scanner sc = new Scanner(System.in);
-     * int n = sc.nextInt();
-     * int sum = 0;
-     * for (int i = 0; i <= n; i++) {
-     * sum = sum + i;
-     * }
-     * System.out.println(sum);
-     */
-    // for (int i = 0; i < 11; i++) {
-    // System.out.println(i);
-    // }
+    
+      if (i == 1 || j == 1 || i == n || j == n) {
+     System.out.print("*");
+      } else {
+      System.out.print(" ");
+      
+      }
+      }
+     System.out.println();
+      
+      }
+      
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int sum = 0;
+      for (int i = 0; i <= n; i++) {
+      sum = sum + i;
+      }
+      System.out.println(sum);
+     
+     for (int i = 0; i < 11; i++) {
+     System.out.println(i);
+     }
 
-    // int i = 0;
-    // while (i < 11) {
-    // System.out.println(i);
-    // i = i + 1;
-    // }
+     int i = 0;
+     while (i < 11) {
+     System.out.println(i);
+     i = i + 1;
+     }
 
-    /*
-     * int i = 0;
-     * do {
-     * System.out.println(i);
-     * i = i + 1;
-     * } while (i < 11);
-     */
+    
+      int i = 0;
+      do {
+      System.out.println(i);
+      i = i + 1;
+      } while (i < 11);
+     
 
 }
