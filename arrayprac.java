40d764b0ffc26b341ea8ad6c8ba09afa9f225ed4
Ketaki Chandak <159540699+ketaki1904@users.noreply.github.com>
2024-10-15 23:05:09 +0530

    Add files via upload

---
+++ arrayprac.java
@@ -0,0 +1,30 @@
+public class arrayprac {
+
+    public static void main(String[] args) {
+
+        // largest elements in array
+        int arr[] = { 1, 2, 5, 4, 3 };
+
+        // sort element by array
+
+        // for (int i = 1; i < arr.length; i++) {
+        // if (arr[i] > arr[i - 1]) {
+
+        // } else {
+        // System.out.println(false);
+        // }
+
+    }
+}
+
+// print all element of array
+// for (int i = 0; i <= arr.length; i++) {
+// System.out.println(arr[i]);
+// }
+// int largest = 0;
+// for (int i = 0; i < arr.length; i++) {
+// if (arr[i] > largest) {
+// largest = arr[i];
+// }
+// }
+// System.err.println(largest);
