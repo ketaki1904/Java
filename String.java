4094d5494344c3ed856afca34aa10b7986eaf08a
Ketaki Chandak <159540699+ketaki1904@users.noreply.github.com>
2024-10-20 23:41:39 +0530

    Add files via upload

---
+++ String.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class String {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("your name is:" + name);
+    }
+}
