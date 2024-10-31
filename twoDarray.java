9d1c2ef62c0e4da0f7fae2dd94687d649f563fd1
Ketaki Chandak <159540699+ketaki1904@users.noreply.github.com>
2024-10-31 22:14:15 +0530

    twoDarray.java

--- twoDarray.java
+++ twoDarray.java
@@ -1,3 +1,37 @@
-public class 2Darray {
-    
+import java.util.*;
+
+public class twdarray {
+    public static void main(String args[]) {
+        Scanner Sc = new Scanner(System.in);
+        int rows = Sc.nextInt();
+        int col = Sc.nextInt();
+
+        int numbers[][] = new int[rows][col];
+
+        // input
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < col; j++) {
+                numbers[i][j] = Sc.nextInt();
+            }
+        }
+
+        int x = Sc.nextInt();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < col; j++) {
+                if (numbers[i][j] == x) {
+                    System.out.print("x is found at the location " + i + "," + j + ")");
+                }
+            }
+        }
+
+        // output statement
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
 }
