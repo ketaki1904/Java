ac797b35fe8c488c449ca471d7bfb08c763e8730
Ketaki Chandak <159540699+ketaki1904@users.noreply.github.com>
2025-01-25 11:10:59 +0530

    Add files via upload

---
+++ IncrementDecrement.java
@@ -0,0 +1,14 @@
+class IncrementDecrement{
+public static void main(String args[]){
+	/*int a=123;
+	//a++;
+	a--;
+	System.out.println(a);*/
+
+	int a=23;	
+	//int b=++a;
+	int b=a++;
+	System.out.println(a);
+	System.out.println(b);
+}
+}
