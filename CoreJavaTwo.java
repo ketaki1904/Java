80b4e4fcce27ea279a608b18ef59e4fb4567356c
Ketaki Chandak <159540699+ketaki1904@users.noreply.github.com>
2025-02-03 23:18:45 +0530

    Add files via upload

---
+++ CoreJavaTwo.java
@@ -0,0 +1,33 @@
+//core java lecture two
+//to declere the variable:
+import java.util.Scanner;
+class CoreJavaTwo{
+public static void main(String args[]){
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter Any String values");
+//DECLEAR FOR ANY INTEGER VALUE
+	/*int no;
+	no=sc.nextInt();*/
+
+//DECLEAR FOR THE FLOAT VALUES IN THE VARIABLE
+	/*float no;
+	no=sc.nextFloat();*/
+
+  //FOR SINGLE STRING TO DECLEAR THE VARIABLE
+	/*String var;
+	var=sc.next();*/
+//FOR DECLEAR THE MULTIPLE STRING AT THE TIME
+	/*String str;
+	str=sc.nextLine();*/
+
+//FOR ANY CHARACTER TO DECLEAR THE VARIABLE
+	/*char ch;
+	ch=sc.next().charAt(0);*/
+
+	System.out.println("my given number is"  +str);
+	//System.out.println("Squre of given number is"  +no*no);
+	//System.out.println("cube of given number is"  +no*no*no);
+
+
+}
+}
