221328d561d46a578f7cb9e2264063724b776a15
Ketaki Chandak <159540699+ketaki1904@users.noreply.github.com>
2025-03-11 16:54:00 +0530

    Employee.java

--- Collectionproject/Employee.java
+++ Collectionproject/Employee.java
@@ -1,1 +1,97 @@
 
+public class Employee {
+	private int id;
+    private String name;
+    private String address;
+    private String mobile;
+    private String email;
+    private String department;
+    private double salary;
+    String city;
+    private boolean isActive;
+    int age;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Employee(int id, String name, String address, String mobile, String email, String department, double salary,
+			String city, boolean isActive, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.mobile = mobile;
+		this.email = email;
+		this.department = department;
+		this.salary = salary;
+		this.city = city;
+		this.isActive = isActive;
+		this.age = age;
+	}
+	public Employee() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", address=" + address + ", mobile=" + mobile + ", email="
+				+ email + ", department=" + department + ", salary=" + salary + ", city=" + city + ", isActive="
+				+ isActive + ", age=" + age + "]";
+	}
+    
+}
