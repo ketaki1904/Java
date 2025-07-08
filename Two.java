ca3bcc853c6c091bf9fd63da39ac7c218843633e
Ketaki Chandak <159540699+ketaki1904@users.noreply.github.com>
2025-07-08 23:08:50 +0530

    Two.java

--- Two.java
+++ Two.java
@@ -1,1 +1,22 @@
+package USS;
+
+public class Two {
+
+    public static void main(String[] args) {
+
+        // Lambda expression to implement run() method
+        Ianimal one = (int j, int k) -> System.out.println("Sum = " + (j + k));
+
+        // Call the lambda
+        one.run(100, 1000);  // Output: Sum = 1100
+    }
+}
+
+@FunctionalInterface
+interface Ianimal {
+    void run(int i, int j);
+    
+    // ❌ If you uncomment below, it breaks the functional interface rule
+    // void sleep();
+}
 
